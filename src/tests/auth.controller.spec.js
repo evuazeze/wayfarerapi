3543c17844b58cadae153622f3d74626979d3f05
Evuazeze Emmanuel Oghenewegba <evuazeze.emmanuel@gmail.com>
2019-07-15 21:29:48 +0100

    test(unit-tests):setup test framework + write unit tests for all endpoints

---
+++ src/tests/auth.controller.spec.js
@@ -0,0 +1,52 @@
+const sinon = require('sinon');
+const chai = require('chai');
+
+chai.should();
+
+describe('Sign up and Sign in Tests', () => {
+  describe('POST /auth/signup', () => {
+    it('should be able to sign up new user', () => {
+      // eslint-disable-next-line no-unused-vars
+      const req = {
+        body: {
+          email: 'evuazeze@gmail.com',
+          first_name: 'Emmanuel',
+          last_name: 'Evuazeze',
+          password: '123',
+        },
+      };
+
+      const res = {
+        status: sinon.spy(),
+        send: sinon.spy(),
+      };
+
+      res.status.calledWith(201).should.equal(true);
+      res.send.calledOnce.should.equal(true);
+    });
+  });
+
+  describe('POST /auth/signin', () => {
+    it('should be able to sign in user', () => {
+      // eslint-disable-next-line no-unused-vars
+      const req = {
+        body: {
+          email: 'evuazeze@gmailcom',
+          password: '123',
+        },
+      };
+
+      const res = {
+        status: sinon.spy(),
+        data: {
+          user_id: sinon.spy(),
+          is_admin: sinon.spy(),
+          token: sinon.spy(),
+        },
+      };
+
+      res.status.calledWith(200).should.equal(true);
+      res.send.calledOnce.should.equal(true);
+    });
+  });
+});
---
+++ src/tests/trip.controller.spec.js
@@ -0,0 +1,51 @@
+const sinon = require('sinon');
+const chai = require('chai');
+
+chai.should();
+
+
+describe('Trip Tests', () => {
+  describe('POST /trips', () => {
+    it('admin should be able to create a trip', () => {
+    // eslint-disable-next-line no-unused-vars
+      const req = {
+        body: {
+          token: '123',
+          user_id: 1,
+          is_admin: false,
+          data: {
+
+          },
+        },
+      };
+
+      const res = {
+        status: sinon.spy(),
+        data: {
+          trip_id: sinon.spy(),
+          bus_id: sinon.spy(),
+          origin: sinon.spy(),
+          destination: sinon.spy(),
+          trip_date: sinon.spy(),
+          fare: sinon.spy(),
+        },
+      };
+
+      res.status.calledWith(201).should.equal(true);
+    });
+  });
+
+  describe('GET /trips', () => {
+    it('user and admin should be able to retrieve trips', () => {
+      // eslint-disable-next-line no-unused-vars
+      const req = {};
+
+      const res = {
+        status: sinon.spy(),
+        send: sinon.spy(),
+      };
+
+      res.status.calledWith(200).should.equal(true, 'Successfully sent response');
+    });
+  });
+});
