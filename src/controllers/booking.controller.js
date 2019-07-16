256e6c4f812c36b4894b1acc83799d9d30ab172d
Evuazeze Emmanuel Oghenewegba <evuazeze.emmanuel@gmail.com>
2019-07-16 20:09:10 +0100

    feature(endpoint):complete user POST booking endpoint

--- src/controllers/booking.controller.js
+++ src/controllers/booking.controller.js
@@ -1,8 +1,9 @@
 const bookingController = (db) => {
   const bookSeat = async (req, res) => {
-    return res.status(200).send('got to the end');
     const tripId = req.body.trip_id;
     const busId = req.body.bus_id;
+    const userId = req.body.user_id;
+    const seatNo = req.body.seat_number;
 
     let hasErrors = false;
 
@@ -15,52 +16,64 @@ const bookingController = (db) => {
       return res.send({ status: 400, error: 'Bad Request Data' });
     }
 
-
-
-    // if (!Object.prototype.hasOwnProperty.call(tripData, 'status')) {
-    //   tripData.status = 1;
-    // }
-    //
     // let text = 'SELECT id FROM "bus" WHERE id = $1';
-    // let values = [tripData.bus_id];
-    // let result;
+    // let values = [busId];
     //
-    // result = await db.query(text, values);
+    // const busResult = await db.query(text, values);
     //
-    // const busRows = result.rows;
+    // const bus = busResult.rows[0];
     //
-    // if (!busRows[0]) return res.status(404).send({ status: 404, error: 'Bus does not exist' });
+    // if (!bus) return res.status(404).send({ status: 404, error: 'No such bus' });
     //
-    // text = 'INSERT INTO "trip"(bus_id, origin, destination, trip_date, fare, status) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
-    // // eslint-disable-next-line max-len
-    // values = [tripData.bus_id, tripData.origin, tripData.destination, new Date(tripData.trip_date), tripData.fare, tripData.status];
+    // text = 'SELECT id FROM "trip" WHERE id = $1';
+    // values = [tripId];
     //
-    // result = await db.query(text, values);
+    // const tripsResult = await db.query(text, values);
     //
-    // const tripRows = result.rows;
+    // const trip = tripsResult.rows[0];
     //
-    // if (!tripRows) {
-    //   res.status(500);
-    //   return res.send({ status: 500, error: 'Error saving trip' });
-    // }
-    //
-    // const savedTrip = tripRows[0];
-    //
-    // const response = {
-    //   status: 201,
-    //   data: {
-    //     trip_id: savedTrip.id,
-    //     bus_id: savedTrip.bus_id,
-    //     origin: savedTrip.origin,
-    //     destination: savedTrip.destination,
-    //     trip_date: savedTrip.trip_date,
-    //     fare: savedTrip.fare,
-    //     status: savedTrip.status,
-    //   },
-    // };
-    //
-    // res.status(201);
-    // return res.send(response);
+    // if (!trip) return res.status(404).send({ status: 404, error: 'No such trip' });
+
+    let text = 'SELECT first_name, last_name, email FROM "user" WHERE id = $1';
+    let values = [userId];
+
+    const userResult = await db.query(text, values);
+
+    const user = userResult.rows[0];
+
+    if (!user) return res.status(500).send({ status: 500, error: 'Error Booking a Seat' });
+
+
+    text = 'INSERT INTO "booking"(trip_id, user_id, created_on, seat_number) VALUES($1, $2, $3, $4) RETURNING *';
+    // eslint-disable-next-line max-len
+    values = [tripId, userId, new Date(), seatNo];
+
+    const insertedBookingResult = await db.query(text, values);
+
+    const booking = insertedBookingResult.rows[0];
+
+    if (!booking) {
+      res.status(500);
+      return res.send({ status: 500, error: 'Error Booking a seat' });
+    }
+
+    const response = {
+      status: 201,
+      data: {
+        booking_id: booking.id,
+        user_id: userId,
+        trip_id: tripId,
+        bus_id: busId,
+        trip_date: booking.created_on,
+        seat_number: seatNo,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+      },
+    };
+
+    res.status(200);
+    return res.send({ status: 201, data: response });
   };
 
   return { bookSeat };
